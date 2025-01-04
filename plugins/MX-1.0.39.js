7de56005cfe86ecac2af2124b76fe6338d833b23
MX-GΔMΞCØDΞR <150932410+themxgamecoder@users.noreply.github.com>
2025-01-04 10:54:40 +0100

    hello world

--- plugins/MX-1.0.39.js
+++ plugins/MX-1.0.39.js
@@ -1,1 +1,22 @@
+const { smd } = require("../lib");
 
+smd(
+  {
+    cmdname: "cats",
+    desc: "Send Images of randome Cats!",
+    type: "fun",
+    filename: __filename,
+  },
+  async (m) => {
+    try {
+      await m.send(
+        "https://cataas.com/cat",
+        { caption: "*meyaoooooooooooooon!*" },
+        "img",
+        m
+      );
+    } catch (e) {
+      m.error(`${e}\n\nCommand: cat`, e, false);
+    }
+  }
+);
